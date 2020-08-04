bdbfae5a7a82ba5d3188b515f404dfd33f261055
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-08-04 11:34:18 -0600

    1-basic-layout

--- Edd1Proyecto1.cpp
+++ Edd1Proyecto1.cpp
@@ -1,16 +1,60 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
-using namespace std;
+using std::cout;
+using std::endl;
+using std::cin;
+
+//Trabajar con listas
+void listas();
+
+//Trabajar con pilas
+void pilas();
+
+//Trabajar con Colas
+void colas();
 
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    bool loop = true;
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+    while(loop) {
+
+        cout << "*** MENU PRINCIPAL ***" << endl
+            << "----------------------" << endl
+            << " [1] Trabajar con Listas" << endl
+            << " [2] Trabajar con Pilas" << endl
+            << " [3] Trabajar con Colas" << endl
+            << " [4] Salir" << endl
+            << "Su eleccion: ";
+    
+        int choice = 0;
+        cin >> choice;
+
+        while (!cin || (choice < 1 || choice > 4))
+        {
+            cin.clear();
+            cin.ignore(100,'\n');
+            cout << "Por favor escoja un numero valido";
+            cin >> choice;
+        }
+    
+        switch (choice) {
+            case 1: {
+              listas();
+               break;
+            }
+            case 2: {
+               pilas();
+               break;
+            }
+            case 3: {
+                colas();
+                break;
+            }
+            case 4: {
+                loop = false;
+                break;
+            }
+        }
     }
-    cout << endl;
 }
