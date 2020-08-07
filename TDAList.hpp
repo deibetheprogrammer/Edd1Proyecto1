e6c1db074412fc6c71cc958d6d1e3fb3518cc765
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-08-07 11:25:00 -0600

    12-tdalista

---
+++ TDAList.hpp
@@ -0,0 +1,34 @@
+#ifndef TDALIST_H
+#define TDALIST_H
+
+#include "Object.h"
+
+class TDAList
+{
+	public:
+		virtual bool inserta(int,Object*)=0;
+		
+		virtual Object* suprime(int)=0;
+		
+		virtual void anula()=0;
+		
+		virtual Object* recupera(int) =0;
+		
+		virtual Object* siguiente(int)=0;
+		
+		virtual Object* anterior(int)=0;
+		
+		virtual bool vacia()=0;
+		
+		virtual void imprime()=0;
+		
+		virtual Object* primero()=0;
+		
+		virtual int localiza(Object*)=0;
+		
+		
+		
+	protected:
+};
+
+#endif
