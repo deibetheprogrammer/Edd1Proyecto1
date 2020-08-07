8e9f7c5452fefa0f688b5c720653c15297392885
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-08-07 11:24:17 -0600

    14-tdacola

---
+++ TDAQueue.hpp
@@ -0,0 +1,18 @@
+#ifndef TDAQUEUE_H
+#define TDAQUEUE_H
+
+#import "Object.h"
+
+class TDAQueue
+{
+	public:
+		virtual Object* frente()=0;
+		virtual void poneEnCola(Object*)=0;
+		virtual Object* quitaDeCola()=0;
+		virtual bool vacia()=0;
+		virtual void anula()=0;
+		virtual void imprime()=0;	
+	protected:
+};
+
+#endif
