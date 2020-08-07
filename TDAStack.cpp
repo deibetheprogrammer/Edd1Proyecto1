a12bd933eb3a1c6eb21a6ac5b9719b082e5d1820
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-08-07 11:25:51 -0600

    13-tdapila

---
+++ TDAStack.cpp
@@ -0,0 +1,1 @@
+#include "TDAStack.h"
---
+++ TDAStack.hpp
@@ -0,0 +1,18 @@
+#ifndef TDASTACK_H
+#define TDASTACK_H
+
+#include "Object.h"
+
+class TDAStack
+{
+	public:
+		virtual void push(Object*)=0;
+		virtual Object* pop() =0;
+		virtual Object* top() =0;
+		virtual bool isEmpty()=0;
+		virtual void print()=0;
+		virtual void clear()=0;
+	protected:
+};
+
+#endif
