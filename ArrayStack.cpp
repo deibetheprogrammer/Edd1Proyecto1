4d034476fb381db4d8609808f101826eb9ed2932
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-08-14 11:10:03 -0600

    18-arraystack

--- ArrayStack.cpp
+++ ArrayStack.cpp
@@ -1,4 +1,8 @@
 #include "ArrayStack.hpp"
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 ArrayStack::ArrayStack()
 {
@@ -9,7 +13,9 @@ ArrayStack::ArrayStack()
 
 void ArrayStack::push(Object* x){
 	//asignar x a la casilla tope
+	array[tope] = x;
 	//incrementar tope
+	tope++;
 }
 
 bool ArrayStack::isEmpty(){
@@ -17,31 +23,47 @@ bool ArrayStack::isEmpty(){
 }
 
 Object* ArrayStack::pop(){
-	//si no est� vac�a 
+	//si no est� vac�a
 		//recuperar el valor de la casilla tope -1 
 		//asignar null a tope -1
 		//tope--
 		//retornar el valor recuperado
+	if(!isEmpty()) {
+		Object* temp = array[tope-1];
+		array[tope-1] = nullptr;
+		tope--;
+		return temp; 
+	}
 	//else return null
+	return nullptr;
 }
 
 void ArrayStack::print(){
 	//for i = tope -1 hasta 1
 		//imprimir el contenido de la casilla i del array
+	for (size_t i = 0; i < tope; i++)
+	{
+		cout << array[i]->toString() << endl;
+	}
+	
 }
 
 Object* ArrayStack::top() {
 	//si no est� vac�a 
 		//recuperar el valor de la casilla tope -1 
 		//retornar el valor recuperado
+	if (!isEmpty()) 
+	{
+		return array[tope-1];
+	}
 	//else return null
+	return nullptr;
 }
 
 void ArrayStack::clear(){
 	for(int i = 0 ; i < tope;i++ )
 		delete array[i];
-		
-	tope =0;
+	tope = 0;
 }
 
 ArrayStack::~ArrayStack()
