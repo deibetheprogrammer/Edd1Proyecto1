a29b3f423ce9aadec17c944ee887c6216d2d824d
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-08-17 11:32:00 -0600

    5-menu 8-menu

--- Edd1Proyecto1.cpp
+++ Edd1Proyecto1.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <string>
+#include "Alumno.hpp"
+#include "ArrayList.hpp"
+#include "ArrayQueue.hpp"
+#include "ArrayStack.hpp"
+#include "LinkedList.hpp"
+#include "LinkedQueue.hpp"
+#include "LinkedStack.hpp"
+
 
 using std::cout;
 using std::endl;
 using std::cin;
+using std::string;
 
 //Trabajar con listas
 void listas();
@@ -12,6 +22,15 @@ void pilas();
 
 //Trabajar con Colas
 void colas();
+
+//Trabajar con ArrayList
+void arraylist();
+
+//Trabajar con ArrayStack
+void arraystack();
+
+//Trabajar con ArrayQueue
+void arrayqueue();
 
 int main()
 {
@@ -101,4 +120,88 @@ void listas() {
 
     }
 
+}
+
+//Trabajar con pilas
+void pilas() {
+
+    bool loop = true;
+
+    while(loop) {
+
+        cout << "*** MENU: TRABAJAR CON PILAS ***" << endl
+            << "----------------------" << endl
+            << " [1] Trabajar con ArrayStack" << endl
+            << " [2] Trabajar con LinkedStack" << endl
+            << " [3] Regresar al menu principal" << endl
+            << "Su eleccion: ";
+    
+        int choice = 0;
+        cin >> choice;
+
+        while (!cin || (choice < 1 || choice > 3))
+        {
+            cin.clear();
+            cin.ignore(100,'\n');
+            cout << "Por favor escoja un numero valido";
+            cin >> choice;
+        }
+    
+        switch (choice) {
+            case 1: {
+                
+               break;
+            }
+            case 2: {
+                
+                break;
+            }
+            case 3: {
+                loop = false;
+                break;
+            }
+        }
+    }
+}
+
+//Trabajar con Colas
+void colas() {
+
+    bool loop = true;
+
+    while(loop) {
+
+        cout << "*** MENU: TRABAJAR CON COLAS ***" << endl
+            << "----------------------" << endl
+            << " [1] Trabajar con ArrayQueue" << endl
+            << " [2] Trabajar con LinkedQueue" << endl
+            << " [3] Regresar al menu principal" << endl
+            << "Su eleccion: ";
+    
+        int choice = 0;
+        cin >> choice;
+
+        while (!cin || (choice < 1 || choice > 3))
+        {
+            cin.clear();
+            cin.ignore(100,'\n');
+            cout << "Por favor escoja un numero valido";
+            cin >> choice;
+        }
+    
+        switch (choice) {
+            case 1: {
+                
+               break;
+            }
+            case 2: {
+                
+                break;
+            }
+            case 3: {
+                loop = false;
+                break;
+            }
+        }
+    }
 }
