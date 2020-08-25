b630581f62845855314bc4c6e0c34445530b03c7
David Bendeck <45319861+deibetheprogrammer@users.noreply.github.com>
2020-08-24 19:45:52 -0600

    6-trabajar-con-arraystack

--- Edd1Proyecto1.cpp
+++ Edd1Proyecto1.cpp
@@ -151,8 +151,8 @@ void pilas() {
     
         switch (choice) {
             case 1: {
-                
-               break;
+                arraystack();
+                break;
             }
             case 2: {
                 
@@ -458,6 +458,107 @@ void arraylist() {
             }
         }
 
+    }
+
+}
+
+//Trabajar con ArrayStack
+void arraystack() {
+
+    ArrayStack* arrayS = new ArrayStack();
+
+    bool loop = true;
+
+    while(loop) {
+
+        cout << "*** MENU: TRABAJAR CON ARRAYLIST ***" << endl
+            << "----------------------" << endl
+            << " [1] Empujar(push)" << endl
+            << " [2] Sacar(pop)" << endl
+            << " [3] Ver Tope" << endl
+            << " [4] Verificar si esta vacia" << endl
+            << " [5] Imprimir elementos" << endl
+            << " [6] Regresar al menu de pilas" << endl
+            << "Su eleccion: ";
+    
+        int choice = 0;
+        //cin >> choice;
+
+        while (!cin || (choice < 1 || choice > 3))
+        {
+            //cin.clear();
+            //cin.ignore(100,'\n');
+            cout << "Por favor escoja un numero valido";
+            //cin >> choice;
+        }
+    
+        switch (choice) {
+            case 1: {
+
+                cout << "Ingrese un simbolo: ";
+                char simbolo;
+                //cin >> simbolo;
+
+                arrayS->push(new Simbolo(simbolo));
+
+                cout << "Nuevo simbolo ingresado" << endl;
+                
+                break;
+            }
+            case 2: {
+
+                Simbolo* sacado = dynamic_cast<Simbolo*>(arrayS->pop());
+
+                if (sacado)
+                {
+                    cout << "ELEMENTO SACADO: " << endl
+                         << sacado->toString() << endl;
+                }
+                else
+                {
+                    cout << "La pila esta vacia";
+                }
+                
+                break;
+            }
+            case 3: {
+
+                Simbolo* tope = dynamic_cast<Simbolo*>(arrayS->top());
+
+                if (tope)
+                {
+                    cout << "ELEMENTO SACADO: " << endl
+                         << tope->toString() << endl;
+                }
+                else
+                {
+                    cout << "La pila esta vacia";
+                }
+
+                break;
+            }
+            case 4: {
+
+                if(arrayS->isEmpty())
+                    cout << "La pila esta vacia";
+                else
+                    cout << "La pila no esta vacia";
+
+                break;
+            }
+            case 5: {
+
+                cout << "Elementos de la pila: " << endl;
+
+                arrayS->print();
+
+                break;
+            }
+            case 6: {
+                loop = false;
+                break;
+            }
+        }
     }
 
 }
